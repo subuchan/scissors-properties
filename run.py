c29cfc6139a46213c0a5a4d5a7e1dda1b139b7b7
subuchan <112760869+subuchan@users.noreply.github.com>
2025-06-16 16:39:21 +0530

    run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from app import create_app
+from flask import Flask
+from flask_cors import CORS
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# Allow only your production frontend domain
+CORS(app, origins=["https://scissorsproperties.com"], supports_credentials=True)
